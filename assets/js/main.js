89b3595cf515d2252c694e4a5954cae9aaeb3162
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-02-04 00:41:37 +0530

    add gobold font and change typing text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 const lander_animation = document.getElementById("lander-animation");
 setTimeout(() => {
-    lander_animation.style.display = 'block';
+    lander_animation.style.display = 'flex';
+    // typing animation
+    const typing_text = document.querySelector('.typing-text');
+    const text = 'BHARAT on the Moon!';
+    let new_text = '', counter = 0;
+    let typing_speed = (1400 / text.length);
+    
+    const typingInterval = setInterval(() => {
+        new_text += text.charAt(counter++);
+        typing_text.textContent = new_text;
+
+        if (counter === text.length) {
+            clearInterval(typingInterval);
+        }
+    }, typing_speed);
+
     setTimeout(() => {
         lander_animation.style.opacity = '0';
         setTimeout(() => {
