95a28032113bd35bd83e463632bbe280411a61b6
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-02-06 09:03:29 +0530

    add scroll to top btn

--- assets/js/universal.js
+++ assets/js/universal.js
@@ -6,3 +6,24 @@ setTimeout(() => {
         loading_page.style.display = 'none';
     }, 1000);
 }, 4000);
+
+const scroll_to_top_btn = document.getElementById('scroll-to-top-btn');
+window.addEventListener("scroll", () => {
+    if (window.scrollY > document.getElementById('About').getBoundingClientRect().top - 160) {
+        scroll_to_top_btn.style.display = 'block';
+        setTimeout(() => {
+            scroll_to_top_btn.style.opacity = '1';
+        }, 500);
+    } else {
+        setTimeout(() => {
+            scroll_to_top_btn.style.display = 'none';
+        }, 200);
+        scroll_to_top_btn.style.opacity = '0';
+    }
+});
+scroll_to_top_btn.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
